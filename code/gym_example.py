eaba4a826eb5121e5a4c810c5c7ccba72c2d0f24
Jiyun Hyo <jiyunhyo@Jiyuns-MBP.lan>
2022-10-10 17:56:19 -0400

    added

--- code/gym_example.py
+++ code/gym_example.py
@@ -1,12 +1,12 @@
 import gym
-env = gym.make("LunarLander-v2", render_mode="human")
+env = gym.make('CartPole-v1', render_mode="human")
 env.action_space.seed(42)
 
 observation, info = env.reset(seed=42)
 
 for _ in range(1000):
     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
+    print(reward)
     if terminated or truncated:
         observation, info = env.reset()
 
